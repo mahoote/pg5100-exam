bdf7556bb449cbc882a1e64bb0a04ae015d660a8
Martin Teigen <martin@teigen.biz>
2022-04-25 14:32:29 +0200

    Refactor fix

--- src/main/kotlin/no/kristiania/pg5100exam/services/user/AuthService.kt
+++ src/main/kotlin/no/kristiania/pg5100exam/services/user/AuthService.kt
@@ -1,6 +1,5 @@
 package no.kristiania.pg5100exam.services.user
 
-import no.kristiania.pg5100exam.controllers.user.AuthorityInfo
 import no.kristiania.pg5100exam.models.user.AuthorityEntity
 import no.kristiania.pg5100exam.repos.user.AuthorityRepo
 import org.springframework.beans.factory.annotation.Autowired
