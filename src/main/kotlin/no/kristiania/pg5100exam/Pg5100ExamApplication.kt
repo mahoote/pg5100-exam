6b2d1678aa9a8a0bfe64a872b2cd799cd3776fa4
Martin Teigen <martin@teigen.biz>
2022-04-25 11:23:11 +0200

    Initial commit

---
+++ src/main/kotlin/no/kristiania/pg5100exam/Pg5100ExamApplication.kt
@@ -0,0 +1,11 @@
+package no.kristiania.pg5100exam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Pg5100ExamApplication
+
+fun main(args: Array<String>) {
+	runApplication<Pg5100ExamApplication>(*args)
+}
